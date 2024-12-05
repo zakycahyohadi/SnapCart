0871946778e1b52e85d49294993de5e5e01ee78e
zakycahyohadi <zakycahyohadi@gmail.com>
2024-12-05 22:38:20 +0700

    Add new YAML file

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "094e2a666391c21873cad3b0e6507842",
+const RESOURCES = {"flutter_bootstrap.js": "3f4613e9de3e171d7ba5f13f324abf05",
 "version.json": "18427138f10211c19fc791cfd4c5c910",
 "index.html": "b1ef1064f310b205d9551d635cc84234",
 "/": "b1ef1064f310b205d9551d635cc84234",
