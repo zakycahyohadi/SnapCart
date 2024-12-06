387dd83168a86b7c78c065b45ceb0cd359daad46
zakycahyohadi <zakycahyohadi@gmail.com>
2024-12-06 09:27:33 +0700

    Deploy with Device Preview

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ Future<void> main() async {
   
   runApp(
     DevicePreview(
-      enabled: !kReleaseMode, // Enable only in debug mode
+      enabled: true, // Enable only in debug mode
       builder: (context) => MultiProvider(
         providers: [
           ChangeNotifierProvider(create: (context) => ThemeProvider()),
