9ae13367e27e2d4f02c30a3f3eedb82724ff21b6
zakycahyohadi <zakycahyohadi@gmail.com>
2024-12-06 09:11:49 +0700

    Add new keren file

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "3f4613e9de3e171d7ba5f13f324abf05",
+const RESOURCES = {"flutter_bootstrap.js": "c77ac541265be2bf71507b494015b067",
 "version.json": "18427138f10211c19fc791cfd4c5c910",
 "index.html": "b1ef1064f310b205d9551d635cc84234",
 "/": "b1ef1064f310b205d9551d635cc84234",
